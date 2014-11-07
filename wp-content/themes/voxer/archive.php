906efe912a294ad865f481e75a9f9ef29dd927a1
PatriciaPineda <patriciaquintanilla88@gmail.com>
2014-11-07 12:58:58 -0800

    Fixing image rezising on mobile and takinging out unwanted plugins

--- wp-content/themes/voxer/archive.php
+++ wp-content/themes/voxer/archive.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="row">
 <!-- Row for main content area -->
-	<div class="small-12 large-8 columns" id="main-content" role="main">
+	<div class="small-12 large-8  medium-8 columns" id="main-content" role="main">
 
 	<?php if ( have_posts() ) : ?>
 
--- wp-content/themes/voxer/functions.js
+++ wp-content/themes/voxer/functions.js
@@ -5,13 +5,4 @@ $(document).ready(function() {
     $("#each-post article.type-post header #share i").click(function() {
         $("#header-social").slideToggle("fast");
     });
-    function isSmall() {
-	  return matchMedia(Foundation.media_queries.small).matches &&
-	    !matchMedia(Foundation.media_queries.medium).matches;
-	}
-	$(window).resize(function() {
-		if(isSmall()){
-			location.reload();
-		}
-	})
 });
